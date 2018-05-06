62e0b19b9bad55de3dab172eefa5782101f79de4
Stephen Tueller <stueller@rti.org>
2018-05-06 11:37:58 -0600

    idaho falls check in

--- R/.htp.r
+++ R/.htp.r
@@ -2,18 +2,19 @@
 #'
 
 
-.htp <- function(data, dvs, phase, ids, time, ivs)
+.htp <- function(data, dvs, phase, ids, uid, time, ivs, ivsl)
 {
   # parralelization
   funcs <- c('eds')
   .foreach(funcs)
   pkgs  <- c('gamlss', 'nlme')
 
+  #
+
   # outer loop for dependent variables
   DVout <- list()
   for(dv in dims$DV)
   {
-
     # middle loop for covariates
     IVout <- list()
     for(iv in dims$IV)
@@ -21,7 +22,11 @@
       # set up Palytic object//needs editing for covariates & situation
       # with no phase
       t1 <- Palytic$new(data=data,
-                        fixed  = formula(paste(dvs[dv], '~', time, '*', phase)),
+                        fixed  = formula(paste(
+                                         paste(dvs[dv], '~', time, '*', phase),
+                                         paste(ivs, collapse = '+'),
+                                         ivsl[[iv]],
+                                         collapse = '+')),
                         random = formula(paste('~', time, '|', ids)) )
 
       # inner loop for participants
@@ -31,11 +36,17 @@
         # post processing//do you want it hear? as a method for Palytic objects?
       }
       # post-estimation aggregation
+      names(IDout) <- paste(ids, uid, sep='.')
+      IDout1 <- lapply(IDout, function(x) summary(x))
+      #IDout1 <- lapply(IDout1, functionx(x) x$id=) # fix names
+      IDout1 <- do.call(rbind, IDout1)
 
-      IVout[[ivs[iv]]] <- 0 # aggregated IDout
+      IVout[[ifelse(is.null(ivs[iv]), 1, ivs[iv])]] <- IDout1 # aggregated IDout
     }
     DVout[[dvs[dv]]] <- IVout
   }
+
   # final post-processing
+  return(DVout)
 
 }
--- R/PersonAlytic.r
+++ R/PersonAlytic.r
@@ -1,6 +1,14 @@
+#' Personalytic
 #'
-#'
-#'
+#' @param data
+#' @param ids Character. Name of the ID variable. If left as NULL, an example will run using the Ovary data from the nlme package.
+#' @param dvs Character list of dependent variable name(s).
+#' @param phase Charcter. Name of the phase variable.
+#' @param time Character. Name of the time variable.
+#' @param ivs Character list of independent variables. These will all be used in every analysis.
+#' @param ivsl Character list of independent variables. These will be looped through one at a time.
+#' @param ind.mods Logical. Should individual models be fit for each ID?
+#' @param grp.mod Logical. Should a group level model be fit across all IDs?
 
 PersonAlytic <- function(data=NULL,
                          ids,
@@ -8,6 +16,7 @@ PersonAlytic <- function(data=NULL,
                          phase,
                          time,
                          ivs=NULL,
+                         ivsl=NULL,
                          ind.mods=TRUE,
                          grp.mod =FALSE)
 {
@@ -24,19 +33,27 @@ PersonAlytic <- function(data=NULL,
     time   <- 'TimeSin'
   }
 
-  # unique idss
+  # unique ids
   uids <- unique(data[[ids]])
 
   # dimensions for loops
   ID <- 1:length(uid)
-  IV <- 1:length(ivs); if(is.null(ivs)) IV <- 1
+  IV <- 1:length(ivsl); if(is.null(ivsl)) IV <- 1
   DV <- 1:length(dvs)
-
   dims <- list(ID=ID, IV=IV, DV=DV)
 
-  if( any( lapply(dims, length) > 1) )
+  if( !any( lapply(dims, length) > 1) | grp.mod )
   {
-    .htp()
+    t1 <- Palytic$new(data=data,
+                      fixed  = formula(paste(
+                        paste(dvs, '~', time, '*', phase),
+                        paste(ivs, collapse = '+'),
+                        collapse = '+')),
+                      random = formula(paste('~', time, '|', ids)) )
+    Grp.out <- t1$gamlss()
   }
+  else DVout <- .htp(data, dvs, phase, ids, uid, time, ivs, ivsl)
+
+
 
 }
