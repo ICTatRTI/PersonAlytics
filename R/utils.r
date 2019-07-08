42b902244e6b865129fb6193449cdf8517aba07f
Stephen Tueller <stueller@rti.org>
2019-07-08 14:13:24 -0600

    0.2.7.0 isn't on gitlab but is in the PersonAlyticsSource folder, iterate to 0.2.7.1

--- R/utils.r
+++ R/utils.r
@@ -798,4 +798,22 @@ pwtime <- function(time, phase)
   return(Time)
 }
 
+# # from https://github.com/OuhscBbmc/OuhscMunge/blob/master/R/update-packages-addin.R
+# #' Download and install dependencies
+# #'
+# #' When called in the repo of an R package, its package dependencies are inspected
+# #' and the obsolete ones are updated.  This function is a thin wrapper around
+# #' \code{remotes::update_packages(remotes::dev_package_deps()$package, dependencies=T)}.  Unlike the # 'Update' button in RStudio's 'Packages' panel,
+# #' this function will (a) update from CRAN and remote sources like GitHub and
+# #' (b) not attempt to install local packages that are unrelated to the current package.
+# #'
+# #' @note
+# #' This function only works if run inside a valid package.  It reads the dependencies enumerated in # the package's
+# #' [DESCRIPTION](https://cran.r-project.org/doc/manuals/r-release/R-exts.html#The-DESCRIPTION-file) # file.
+# #'
+# #' @export
+# update_packages_addin <- function() {
+#   dependency_list <- remotes::dev_package_deps()
+#   remotes::update_packages(dependency_list$package, dependencies=TRUE)
+# }
 
--- vignettes/PersonAlytics_Users_Guide.R
+++ vignettes/PersonAlytics_Users_Guide.R
@@ -1,9 +1,21 @@
 ## ----setup, include=FALSE------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
 library(knitr)
-papv <- packageVersion("PersonAlytics")
+library(PersonAlytics)
 
-
+depends <- function()
+{
+  d <- scan('../DESCRIPTION', what = 'character', sep='\n')
+  wd <- which( grepl("*Depends*", d) )
+  wr <- which( grepl("RoxygenNote*", d) )
+  pkgs <- c(d[(wd+1):(wr-1)])
+  pkgs <- gsub( " ", "", pkgs)
+  pkgs <- gsub( "\t", "", pkgs)
+  pkgs <- gsub( ",", "", pkgs)
+  pkgs
+}
+pkgs <- depends()
+pkgss <-  matrix( unlist( strsplit(pkgs, "\\(") ), ncol=2, byrow=TRUE)[,1]
 
 ## ----eval=FALSE----------------------------------------------------------
 #  install.deps <- function(pkg)
@@ -14,12 +26,14 @@ papv <- packageVersion("PersonAlytics")
 #                                          repos = "https://cran.rstudio.com/")
 #    if (length(old.pkg)) update.packages(old.pkg, dependencies = TRUE)
 #  }
-#  packages <- c("gamlss", "ggplot2", "multilevel", "nlme", "R6")
-#  install.deps(packages)
+
+## ---- echo=FALSE, comment=NA---------------------------------------------
+cat(paste("install.deps(c(", paste("'", pkgss[-1], "'", collapse=', ', sep=""), "))", 
+          sep=""), "\n")
 
 ## ---- message=FALSE------------------------------------------------------
 library(PersonAlytics)
 
-## ------------------------------------------------------------------------
-?PersonAlytic
+## ---- eval=FALSE---------------------------------------------------------
+#  ?PersonAlytic
 
